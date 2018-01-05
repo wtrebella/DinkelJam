ca3ab3807e590889dd9a9a0c3d23107550e4766c
Whitaker Trebella <wtrebella@gmail.com>
2018-01-04 23:19:04 -0600

    - created the unity project - made a basic title screen

---
+++ DinkelJam_Unity/Assets/Scripts/TitleController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleController : MonoBehaviour
+{	
+	[SerializeField] private Animator _animator;
+	[SerializeField] private ParticleSystem _particles;
+	[SerializeField] private AudioSource _dinkelJamAudio;
+
+	void Start()
+	{
+		StartCoroutine(ShowTitle());
+	}
+
+	IEnumerator ShowTitle()
+	{
+		yield return new WaitForSeconds(1.0f);
+
+		_animator.SetTrigger("Show");
+	}
+
+	public void OnHitGround()
+	{
+		_particles.Play();
+		_dinkelJamAudio.Play();
+	}
+}
