c938d2083a378abfe5cecc28f9f39e6998d0df00
Whitaker Trebella <wtrebella@gmail.com>
2018-01-06 18:41:09 -0600

    - got statue comparison shooting physics working and looking pretty goo

---
+++ DinkelJam_Unity/Assets/Scripts/ManCutter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManCutter : MonoBehaviour
+{
+   [SerializeField] private FracturedObject _fracturedObject;
+   
+   private List<GameObject> _manChunks;
+   
+   void Awake()
+   {
+
+   }
+   
+   void Update()
+   {
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+         UltimateFracturing.Fracturer.FractureToChunks(_fracturedObject, true, out _manChunks, OnProgress);
+      }
+   }
+   
+   void OnProgress(string title, string message, float alpha)
+   {
+      Debug.Log(title + ": " + message + ", " + alpha);
+   }
+}
