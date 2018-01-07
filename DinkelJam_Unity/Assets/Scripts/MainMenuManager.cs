1a414bb19d1fb21070c3380d08f7d290194229e3
Whitaker Trebella <wtrebella@gmail.com>
2018-01-07 15:33:31 -0600

    - moved test scenes to TestScenes folder - hooked up MainMenu to rest of game, so when you click it'll load the game - got back InitScene and MultisceneLoader, that got lost in an earlier merge - added scenes to build settings

---
+++ DinkelJam_Unity/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour 
+{
+   private float _startTime = 0;
+
+   void Awake()
+   {
+      _startTime = Time.time;  
+   }
+
+   void Update()
+   {
+      float elapsedTime = Time.time - _startTime;
+
+      if (elapsedTime > 1.0f)
+      {
+         if (Input.anyKeyDown || Input.GetMouseButtonDown(0))
+         {
+            MultisceneLoader.instance.LoadGame();
+         }
+      }
+   }
+}
