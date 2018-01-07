ff5de23d11ca950e5b02f9ff2fc45c9b89cd519d
David Laskey <dlaskey92@gmail.com>
2018-01-07 14:49:32 -0600

    Updated player controller to encompass Shooting functionality

--- DinkelJam_Unity/Assets/Scripts/PlayerController.cs
+++ DinkelJam_Unity/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,15 @@ public class PlayerController : MonoBehaviour {
 
 	[SerializeField] float _moveSpeed;
 
+	bool _previouslyShooting = false;
+	float _shootingTime = 0.0f;
+
+	[SerializeField] Transform _cameraTransform;
+	[SerializeField] float _fromCenterForce = 300.0f;
+	[SerializeField] float _fromCenterRadius = 0.1f;
+	[SerializeField] float _fireInterval = 1.0f;
+	float _fireTimer = 0.0f;
+
 	void Update() {
 
 		Vector3 movement = new Vector3(0f, 0f, 0f);
@@ -27,5 +36,39 @@ public class PlayerController : MonoBehaviour {
 		}	
 
 		transform.position += ((transform.right * movement.x) + (transform.forward * movement.z)) * Time.deltaTime;
+
+		FireWeapon();
+	}
+
+	void FireWeapon() {
+
+		bool holdingDownFire = Input.GetKey (KeyCode.Mouse0);
+		if (holdingDownFire) {
+			_shootingTime += Time.deltaTime;
+
+			_fireTimer += Time.deltaTime;
+			if (_fireTimer > _fireInterval) {
+				_fireTimer = 0.0f;
+
+				RaycastHit hitInfo;
+
+				FracturedChunk chunkRaycast = FracturedChunk.ChunkRaycast(_cameraTransform.position, _cameraTransform.forward, out hitInfo);
+				if (chunkRaycast) {
+					chunkRaycast.FromCenterImpact(hitInfo.point, _fromCenterForce, _fromCenterRadius, true);
+				}
+				GameAudio.PlayOneShot("GunShot");
+			}
+		}
+
+		if (!holdingDownFire && _previouslyShooting) {
+			//if the previous shot was true and current is false
+			//the player just stopped shooting
+			GameAudio.PlayOneShot("ShellDrop", new Dictionary<string, float>() {
+				{"Shots", _shootingTime}
+			});
+			_shootingTime = 0.0f;
+		}
+			
+		_previouslyShooting = holdingDownFire;
 	}
 }
