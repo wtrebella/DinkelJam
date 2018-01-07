95430f786e0e95ec0fb78d8475e345e0cec3540f
Whitaker Trebella <wtrebella@gmail.com>
2018-01-07 12:40:42 -0600

    - got a multiscene loader in

---
+++ DinkelJam_Unity/Assets/Scripts/Editor/MultisceneEditor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+[CustomEditor(typeof(Multiscene))]
+public class MultisceneEditor : Editor 
+{
+   Multiscene _multiscene;
+   
+   void OnEnable()
+   {
+      _multiscene = (Multiscene)target;  
+   }
+   
+   public override void OnInspectorGUI()
+   {
+      DrawDefaultInspector();
+            
+      string baseSceneName = _multiscene.GetBaseSceneName();
+      
+      if (baseSceneName != null)
+      {
+         if (GUILayout.Button("Load Scenes"))
+         {
+            _multiscene.LoadScenes();
+         }
+      }
+   }
+}
---
+++ DinkelJam_Unity/Assets/Scripts/Multiscene.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.SceneManagement;
+
+[CreateAssetMenu(fileName = "New Multiscene")]
+public class Multiscene : ScriptableObject 
+{
+   [SerializeField] private string _baseSceneName;
+   [SerializeField] private string[] _additiveSceneNames;
+   [SerializeField] private string _singleScenesPath = "/Scenes/Single Scenes/";
+         
+   public string GetBaseSceneName()
+   {
+      return _baseSceneName;
+   }
+   
+   public string[] GetAdditiveSceneNames()
+   {
+      return _additiveSceneNames;  
+   }
+   
+   [OnOpenAsset()]
+   static bool OnOpenAsset(int instanceID, int line) 
+   {
+      if (!(EditorUtility.InstanceIDToObject(instanceID) is Multiscene)) return false;
+      
+      Multiscene multiscene = (Multiscene)EditorUtility.InstanceIDToObject(instanceID);
+      multiscene.LoadScenes();
+      return true;
+   }
+   
+   string GetScenePath(string sceneName)
+   {
+      return Application.dataPath + _singleScenesPath + sceneName + ".unity";
+   }
+   
+   public void LoadScenes()
+   {
+      if (string.IsNullOrEmpty(_baseSceneName)) return;
+      
+      EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+      EditorSceneManager.OpenScene(GetScenePath(_baseSceneName), OpenSceneMode.Single);
+
+      for (int i = 0; i < _additiveSceneNames.Length; i++)
+      {
+         string additiveSceneName = _additiveSceneNames[i];
+         EditorSceneManager.OpenScene(GetScenePath(additiveSceneName), OpenSceneMode.Additive);
+      }
+   }
+}
