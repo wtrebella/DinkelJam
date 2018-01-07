125eca966683aae998bdc3d83b9b2fe779ae956a
David Laskey <dlaskey92@gmail.com>
2018-01-07 12:21:13 -0600

    Added FMOD banks/settings to project Added wrapper functions for playing FMOD events via GameAudio class

---
+++ DinkelJam_Unity/Assets/Scripts/GameAudio.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using FMODUnity;
+
+public class GameAudio : MonoBehaviour {
+
+	public static void PlayOneShot(string eventName) {
+		PlayOneShot(eventName, default(Vector3));
+	}
+
+	public static void PlayOneShot(string eventName, Vector3 pos) {
+		FMODUnity.RuntimeManager.PlayOneShot("event:/" + eventName, pos);
+	}
+}
