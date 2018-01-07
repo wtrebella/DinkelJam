f9776f0185e0c6b93f825ed4dab41505b7e30147
David Laskey <dlaskey92@gmail.com>
2018-01-06 18:42:30 -0600

    Added player and camera controllers

---
+++ DinkelJam_Unity/Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+
+	[SerializeField] float _xSensitivity;
+	[SerializeField] float _ySensitivity;
+	[SerializeField] Vector2 _yClamp;
+
+	[SerializeField] float _slerpSpeed;
+
+	private float xAngle;
+	private float yAngle;
+
+	void Awake() {
+		Cursor.lockState = CursorLockMode.Locked;
+		xAngle = 0.0f;
+		yAngle = 0.0f;
+	}
+
+	void LateUpdate () {
+		float mod = _xSensitivity * Time.deltaTime;
+		float mouseX = Input.GetAxis("Mouse X") * _xSensitivity * Time.deltaTime;;
+		float mouseY = -Input.GetAxis("Mouse Y") * _ySensitivity * Time.deltaTime;;
+
+		xAngle += mouseX;
+		yAngle += mouseY;
+
+		yAngle = Mathf.Clamp(yAngle, _yClamp.x, _yClamp.y);
+
+		transform.localRotation = Quaternion.Euler(yAngle, 0f, 0f);
+		transform.parent.rotation = Quaternion.Euler(0f, xAngle, 0f);
+	}
+}
---
+++ DinkelJam_Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField] float _moveSpeed;
+
+	void Update() {
+
+		Vector3 movement = new Vector3(0f, 0f, 0f);
+
+		if(Input.GetKey(KeyCode.A)) {
+			movement.x -= _moveSpeed;
+		}
+
+		if(Input.GetKey(KeyCode.D)) {
+			movement.x += _moveSpeed;
+		}
+
+		if(Input.GetKey(KeyCode.S)) {
+			movement.z -= _moveSpeed;
+		}
+
+		if(Input.GetKey(KeyCode.W)) {
+			movement.z += _moveSpeed;
+		}	
+
+		transform.position += ((transform.right * movement.x) + (transform.forward * movement.z)) * Time.deltaTime;
+	}
+}
